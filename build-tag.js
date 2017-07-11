c524db6448f8ec4a8be07e85e2197ee7fe2e9c47
Frank Leng <email@frankleng.me>
2017-07-10 18:04:40 -0700

    remove semver validation, we are simply tagging version with a build number

--- build-tag.js
+++ build-tag.js
@@ -2,20 +2,19 @@
 
 var path = require("path");
 var fs = require("fs");
-var semver = require("semver");
 var yargs = require("yargs")
     .usage('Usage: $0 --tag alpha')
     .demand('tag')
     .alias('t', 'tag')
     .argv;
 
-var tag = yargs.tag;
+var tag = yargs.tag.replace(/\\|\//g, '.');
 var packageJsons = yargs._;
 if (!packageJsons || packageJsons.length === 0) {
     packageJsons = ["./package.json"];
 }
 
-packageJsons.forEach(p => {
+packageJsons.forEach(function(p) {
     var absolutePath = path.resolve(p);
     if (fs.statSync(absolutePath).isDirectory()) {
         absolutePath = path.join(absolutePath, "package.json");
@@ -24,23 +23,13 @@ packageJsons.forEach(p => {
     var packageJson = JSON.parse(fs.readFileSync(absolutePath));
 
     var originalVersion = packageJson.version;
-    if (!semver.valid(originalVersion)) {
-        console.error("Original package.json version is not semver valid: " + taggedVersion + " for package at: " + absolutePath);
-        process.exit(2);
-    }
 
     var originalNonTagVersion = (packageJson.version || "").split("-")[0];
 
     var taggedVersion = originalNonTagVersion + "-" + tag;
-    if (!semver.valid(taggedVersion)) {
-        console.error("Tagged package.json version is not semver valid: " + taggedVersion + " for package at: " + absolutePath);
-        process.exit(2);
-    }
 
     packageJson.version = taggedVersion;
 
     fs.writeFileSync(absolutePath, JSON.stringify(packageJson, null, "    "));
     console.log(packageJson.name + "@" + originalVersion + " -> " + packageJson.name + "@" + taggedVersion + " at " + path.relative(process.cwd(), absolutePath));
 });
-
-
